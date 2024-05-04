2b9fc9efb5f0606acf7b40a7e31b30193e02811c
Taylor Boyd <epicdrak@gmail.com>
2024-05-03 22:36:55 -0700

    Added all of the buttons needed to select the correct notes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #004d40;
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  margin: 0px 2px;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: #009688;
+  }
+`;
 
 function App() {
-  const [data, setData] = useState([{}]);
-
-  useEffect(() => {
-    fetch("/members")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, []);
-
   return (
     <div>
-      {typeof data.members === "undefined" ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member, i) => <p key={i}>{member}</p>)
-      )}
+      <Button>E</Button>
+      <Button>F</Button>
+      <Button>F#</Button>
+      <Button>G</Button>
+      <Button>G#</Button>
+      <Button>A</Button>
+      <Button>A#</Button>
+      <Button>B</Button>
+      <Button>C</Button>
+      <Button>C#</Button>
+      <Button>D</Button>
+      <Button>D#</Button>
     </div>
   );
 }
--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/members")
-def memebers():
+def members():
     return {"members": ["Member1", "Member2", "Member3"]}
 
 if __name__ == "__main__":
