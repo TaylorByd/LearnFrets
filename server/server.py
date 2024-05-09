a609f62fda4a035dc032e4140df36eafb80fd995
Taylor Boyd <epicdrak@gmail.com>
2024-05-08 19:08:07 -0700

    Modified the names of the app.route functions to make more sense.

--- server/server.py
+++ server/server.py
@@ -8,15 +8,16 @@ detectedNote = ""
 random_note = random.choice(random.choice(fretboard_logic.fretboard))
 
 @app.route('/selectednote', methods=['POST', 'GET'])
-def receive_data():
+def getSelectedNote():
     global detectedNote
     selectedNote = request.data.decode()
-    print("Selected Note: ", selectedNote)
     detectedNote = selectedNote
+    print("Selected Note: ", selectedNote)
+    print(request.remote_addr)
     return "Data Received!"
 
 @app.route('/correctnote')
-def received():
+def compareRandomNote():
     global random_note
     global detectedNote
     random_col = random.randint(0,24)
@@ -26,10 +27,10 @@ def received():
         random_note = fretboard_logic.fretboard[random_row][random_col]
         fretboard_img_note_selected.place_dot_img(fretboard_img_note_selected.dot_coordinates[random_row][random_col])
         detectedNote = '0'
-        # used in the case that we get two repeated random notes in a row and we do not want that repeated note to be skipped.
+            # used in the case that we get two repeated random notes in a row and we do not want that repeated note to be skipped.
         return {"bool" : "True"}
     else:
         return {"bool" : "False"}
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    app.run(debug=True)
